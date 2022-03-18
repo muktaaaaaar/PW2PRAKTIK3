80c765e9a0f40f508b84c16c64a29c982d023dc7
muktaaaaaar <92444146+muktaaaaaar@users.noreply.github.com>
2022-03-18 16:19:07 +0700

    Add files via upload

---
+++ praktikum3/startbootstrap-sb-admin-gh-pages/view/artikel1.php
@@ -0,0 +1,43 @@
+<?php
+include '../layouit/header.php';
+?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>
+				Nama-Nama Hewan Yang Hidup di Darat
+			</h3>
+			<ul>
+				<li class="list-item">
+					Ayam
+				</li>
+				<li class="list-item">
+					Babi
+				</li>
+				<li class="list-item">
+					Kuda
+				</li>
+				<li class="list-item">
+					Kambing
+				</li>
+				<li class="list-item">
+					Sapi
+				</li>
+				<li class="list-item">
+                    Kucing
+				</li>
+				<li class="list-item">
+					Anjing
+				</li>
+				<li class="list-item">
+					Harimau
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
+<?php
+include '../layouit/footer.php';
+?>
---
+++ praktikum3/startbootstrap-sb-admin-gh-pages/view/artikel2.php
@@ -0,0 +1,43 @@
+<?php
+include '../layouit/header.php';
+?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>
+				Nama-Nama Ikan
+			</h3>
+			<ul>
+				<li class="list-item">
+					Ikan Lele
+				</li>
+				<li class="list-item">
+					Ikan Hiu
+				</li>
+				<li class="list-item">
+					Ikan Emas
+				</li>
+				<li class="list-item">
+					Ikan Mujair
+				</li>
+				<li class="list-item">
+					Ikan Salmon
+				</li>
+				<li class="list-item">
+                    Ikan Teri
+				</li>
+				<li class="list-item">
+					Ikan Tongkol
+				</li>
+				<li class="list-item">
+					Ikan Cupang
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+
+<?php
+include '../layouit/footer.php';
+?>
